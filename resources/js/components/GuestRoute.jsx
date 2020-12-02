90c610f1ae4a2c1b72890f3389ed43a759ec516b
Loris Moulin <contact@lorismoulin.com>
2020-12-02 20:22:06 +0100

    updated Guest and Private route components

--- resources/js/components/GuestRoute.jsx
+++ resources/js/components/GuestRoute.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
+import { fetchUser } from "../store/userSlice";
 
-const GuestRoute = ({ children, user }) => (
-    <Route
-        render={({ location }) =>
-            user ? (
-                <Redirect
-                    to={{
-                        pathname: "/",
-                        state: { from: location }
-                    }}
-                />
-            ) : (
-                children
-            )
-        }
-    />
-);
+const GuestRoute = ({ children, user, dispatch }) => {
+    useEffect(() => dispatch(fetchUser()));
 
-const mapStateToProps = ({ user }) => ({ user });
+    return (
+        <Route
+            render={({ location }) =>
+                user ? (
+                    <Redirect
+                        to={{
+                            pathname: "/",
+                            state: { from: location }
+                        }}
+                    />
+                ) : (
+                    children
+                )
+            }
+        />
+    );
+};
+
+const mapStateToProps = ({ user }, { children }) => ({ user, children });
 
 export default connect(mapStateToProps)(GuestRoute);
--- resources/js/components/PrivateRoute.jsx
+++ resources/js/components/PrivateRoute.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
+import { fetchUser } from "../store/userSlice";
 
-const PrivateRoute = ({ children, user }) => (
-    <Route
-        render={({ location }) =>
-            user ? (
-                children
-            ) : (
-                <Redirect
-                    to={{
-                        pathname: "/login",
-                        state: { from: location }
-                    }}
-                />
-            )
-        }
-    />
-);
+const PrivateRoute = ({ children, user, dispatch }) => {
+    useEffect(() => dispatch(fetchUser()));
 
-const mapStateToProps = ({ user }) => ({ user });
+    return (
+        <Route
+            render={({ location }) =>
+                user ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: location }
+                        }}
+                    />
+                )
+            }
+        />
+    );
+};
+
+const mapStateToProps = ({ user }, { children }) => ({ user, children });
 
 export default connect(mapStateToProps)(PrivateRoute);
