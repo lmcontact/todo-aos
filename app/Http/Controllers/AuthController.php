efbad047ef0a5652a3b542443b95f3f23c8a14cd
Loris Moulin <contact@lorismoulin.com>
2020-12-02 11:08:29 +0100

    updated Register route logic, added register route to router

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
         $formData = $request->validated();
+
+        User::create([
+            'username' => $formData['username'],
+            'password' => Hash::make($formData['password'])
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])
+        ->name('register');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
